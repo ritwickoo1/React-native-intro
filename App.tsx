efd6c48fd20a6788f0a10f73067ca033036da9ad
Ritwick Verma <21mcmc09@uohyd.ac.in>
2023-02-23 11:59:03 +0530

    first-commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+
+
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Hii, There</Text>
+    </View>
+  );
+}
+
+
+
+export default App;
